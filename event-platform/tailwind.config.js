bdcecb4d08339db661532ba1e2516e7de7c4df9e
Felipe Holanda de Freitas <79938668+felipeholanda2077@users.noreply.github.com>
2022-06-24 00:56:17 -0300

    Aldição de novos estilos

--- event-platform/tailwind.config.js
+++ event-platform/tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        blur: 'url(/src/assets/blur-back.png)'
+      },
       fontFamily: {
         sans: 'Roboto, sans-serif',
       },
@@ -27,7 +30,7 @@ module.exports = {
           100: '#E1E1E6',
           200: '#C4C4CC',
           300: '#8D8D99',
-          500: '#323223B',
+          500: '#32322B',
           600: '#29292E',
           700: '#121214',
           900: '#09090A'
